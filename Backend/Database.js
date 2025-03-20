14f2dc007022704915b2ed61ea9af8ff4487436f
michael <michaelzhou20111@hotmail.com>
2025-03-20 11:51:04 -0400

    initialize project

---
+++ Backend/Database.js
@@ -0,0 +1,6 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  host: '34.124.126.216',
+  database: 'postgre',
+  port: 5432,
+});
---
+++ Frontend/src/Login.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Login.css'
+
+function Login() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>Login</p>
+    </>
+  )
+}
+
+export default Login
