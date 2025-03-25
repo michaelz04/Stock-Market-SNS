f4c1075bb58107b1b19dd51009823029cd43d49f
michael <michaelzhou20111@hotmail.com>
2025-03-25 13:00:56 -0400

    added more routes and components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState, createContext, useContext } from "react";
 import Login from './Login';
 import Home from './Home';
+import Portfolio from './Portfolio';
+import Stocklist from './Stocklist';
+import Friend from './Friend';
 
 export const UserContext = createContext();
 
@@ -19,6 +22,21 @@ function App() {
         <Route path="/home" element={
           <UserContext.Provider value={user}>
             <Home />
+          </UserContext.Provider>
+          } />
+        <Route path="/portfolios" element={
+          <UserContext.Provider value={user}>
+            <Portfolio />
+          </UserContext.Provider>
+          } />
+        <Route path="/stocklists" element={
+          <UserContext.Provider value={user}>
+            <Stocklist />
+          </UserContext.Provider>
+          } />
+        <Route path="/friends" element={
+          <UserContext.Provider value={user}>
+            <Friend />
           </UserContext.Provider>
           } />
       </Routes>
