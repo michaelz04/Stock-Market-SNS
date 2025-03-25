08b126dcc705dfb504b0c99e292dcadde43631a4
michael <michaelzhou20111@hotmail.com>
2025-03-25 13:13:09 -0400

    added logout

--- Frontend/src/Navbar.jsx
+++ Frontend/src/Navbar.jsx
@@ -7,6 +7,7 @@ function Navbar(){
                 <li><a href="/portfolios">Portfolios</a></li>
                 <li><a href="/stocklists">Stock Lists</a></li>
                 <li><a href="/friends">Friends</a></li>
+                <li><a href="/">Logout</a></li>
             </ul>
         </nav>
     )
